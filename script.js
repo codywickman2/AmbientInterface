c0c5b0f85fb83c3ca004faba7ebbcc6466629604
codywickman2 <49879926+codywickman2@users.noreply.github.com>
2020-12-02 18:29:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-function weatherAPI() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Fresno,us&appid=7f1759c88a7dded2a4f0d123937ba813')
+function run() {
+    myFunction();
+    getLocation();
+}
+var x = document.getElementById("lat");
+var aud = document.getElementById("rain");
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(weatherAPI);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+function weatherAPI(position) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&appid=7f1759c88a7dded2a4f0d123937ba813')
         .then(response => response.json())
         .then(data => {
             var weather = document.getElementById("weather")
@@ -8,14 +40,12 @@ function weatherAPI() {
 
             var tempFahrenheit = (currentTemp.temp) * 9/5 - 459.67
             var p = document.createElement('p')
-            var tNode = document.createTextNode(tempFahrenheit.toFixed(1))
+            var tNode = document.createTextNode(tempFahrenheit.toFixed(0))
             p.appendChild(tNode)
             weather.appendChild(p)
 
-            if (currentTemp.humidity > 50) {
-                var sound = document.getElementById("rain");
-                sound.play()
+            if (tempFahrenheit > 50) {
+                aud.play();
             }
         })
-
 }
